58e89d085beed41e9c9534990bfabaa2ded3dbfb
Joshua Buck <joshua@edgeimpulse.com>
2023-04-27 09:47:32 -0500

    testing accel - photon2 sends data, but not data back from accel

--- src/pei.ino
+++ src/pei.ino
@@ -1,92 +1,20 @@
-#include <stdio.h>
-#define EI_PORTING_ARDUINO
-#define __STATIC_FORCEINLINE
-#define __STATIC_INLINE
-#define __ASM
+//try out the accelerometer
 
+#include "ADXL362.h"
 
+ADXL362 adxl362;
 
-#include "edge-impulse-sdk/classifier/ei_run_classifier.h"
-
-// Callback function declaration
-static int get_signal_data(size_t offset, size_t length, float *out_ptr);
-
-// Raw features copied from test sample
-static const float features[] = {
-    1.3172, -0.1727, -1.3843, 1.7894, 0.7405, -0.0164, -0.1300, -0.8644, -0.3681, -0.2890, -0.6120, 1.2273, 0.0641, -0.8514, 1.5991, 0.5015, 0.2955, 0.6299, 0.2924, 0.4547, 0.0134, 0.2741, 4.1152, -0.0110, -0.5401, 2.6704, 1.6849, 1.5769, 1.1993, 1.2901, 1.3384, 1.1790, 0.3214
-};
-
-int main(int argc, char **argv) {
-
-    signal_t signal;            // Wrapper for raw input buffer
-    ei_impulse_result_t result; // Used to store inference output
-    EI_IMPULSE_ERROR res;       // Return code from inference
-
-    // Calculate the length of the buffer
-    size_t buf_len = sizeof(features) / sizeof(features[0]);
-
-    // Make sure that the length of the buffer matches expected input length
-    if (buf_len != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
-        ei_printf("ERROR: The size of the input buffer is not correct.\r\n");
-        ei_printf("Expected %d items, but got %d\r\n",
-                EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE,
-                (int)buf_len);
-        return 1;
-    }
-
-    // Assign callback function to fill buffer used for preprocessing/inference
-    signal.total_length = EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE;
-    signal.get_data = &get_signal_data;
-
-    // Perform DSP pre-processing and inference
-    res = run_classifier(&signal, &result, false);
-
-    // Print return code and how long it took to perform inference
-    ei_printf("run_classifier returned: %d\r\n", res);
-    ei_printf("Timing: DSP %d ms, inference %d ms, anomaly %d ms\r\n",
-            result.timing.dsp,
-            result.timing.classification,
-            result.timing.anomaly);
-
-    // Print the prediction results (object detection)
-#if EI_CLASSIFIER_OBJECT_DETECTION == 1
-    ei_printf("Object detection bounding boxes:\r\n");
-    for (uint32_t i = 0; i < result.bounding_boxes_count; i++) {
-        ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
-        if (bb.value == 0) {
-            continue;
-        }
-        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
-                bb.label,
-                bb.value,
-                bb.x,
-                bb.y,
-                bb.width,
-                bb.height);
-    }
-
-    // Print the prediction results (classification)
-#else
-    ei_printf("Predictions:\r\n");
-    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
-        ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
-        ei_printf("%.5f\r\n", result.classification[i].value);
-    }
-#endif
-
-    // Print anomaly result (if it exists)
-#if EI_CLASSIFIER_HAS_ANOMALY == 1
-    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
-#endif
-
-    return 0;
+void setup()
+{
+   adxl362.begin();
 }
 
-// Callback: fill a section of the out_ptr buffer when requested
-static int get_signal_data(size_t offset, size_t length, float *out_ptr) {
-    for (size_t i = 0; i < length; i++) {
-        out_ptr[i] = (features + offset)[i];
-    }
+void loop()
+{
+  int x, y, z;
 
-    return EIDSP_OK;
+  adxl362.readXYZmg(x, y, z);
+
+  Serial.printlnf("x: %d, y: %d, z: %d", x, y, z);
+  delay(500);
 }
