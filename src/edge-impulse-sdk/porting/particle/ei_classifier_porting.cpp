829b9b8ff6eec94d56e2efff61762924ce50870b
automatiek <arjan.kamphuis@gmail.com>
2023-05-24 13:09:33 +0200

    Standalone example working

--- src/edge-impulse-sdk/porting/particle/ei_classifier_porting.cpp
+++ src/edge-impulse-sdk/porting/particle/ei_classifier_porting.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 EdgeImpulse Inc.
+ * Copyright (c) 2023 EdgeImpulse Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
